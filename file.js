72400932c1df82a0622b9b3e81bae32513bb9f3b
Satavisa Das <satavisa1234@gmail.com>
2024-05-20 13:52:02 +0530

    Learn

--- file.js
+++ file.js
@@ -1,14 +1,19 @@
 const fs=require('fs');
-fs.writeFileSync('./test.txt','Hey there!');
-fs.writeFile('./test.txt','Hey there!',(err)=>{});
+//fs.writeFileSync('./test.txt','Hey there!');
+//fs.writeFile('./test.txt','Hey there!',(err)=>{});
+//Blocking operation
+console.log("1");
  const result=fs.readFileSync('./contacts.js','utf-8');
  console.log(result);
+ console.log("2");
+console.log("\n");
+ //Non-blocking operation
+ console.log("1");
 fs.readFile('./contacts.js','utf-8',(err,result)=>{
-  if(err)
-    console.log("Error",err);
-  else  
+   
     console.log(result);
 });
-fs.appendFileSync('./test.txt',new Date().getDate().toLocaleString());
-fs.cpSync('./test.txt','./copy.txt');
+console.log("2");
+//fs.appendFileSync('./test.txt',new Date().getDate().toLocaleString());
+//fs.cpSync('./test.txt','./copy.txt');
 
